b638419169f761371d46d81166d545b067a1b879
Geoff Hulette <ghulette@gmail.com>
2011-11-09 15:33:54 -0800

    Update

--- code/CountWords.hs
+++ code/CountWords.hs
@@ -1,20 +1,16 @@
 import IO
+import System (getArgs)
 import Data.Char
-
--- pureFunction :: Int -> Int
--- pureFunction n = do
---   print n
---   return (n + 1)
 
 countWordsInFile :: FilePath -> IO Int
 countWordsInFile f = do
   h <- openFile f ReadMode
   s <- hGetContents h
-  let ws = words s
-  return (length ws)
+  return ((length . words) s)
 
 main :: IO ()
 main = do
-  n <- countWordsInFile "fib.hs"
+  [file] <- getArgs
+  n <- countWordsInFile file
   let msg = "I counted " ++ (show n) ++ " words"
   putStrLn msg
